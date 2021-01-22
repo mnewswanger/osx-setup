933f9056fa44325ab5c7ba9926352e15bf6cde23
Mike Newswanger <mike@mikenewswanger.com>
2021-01-22 16:24:54 -0500

    Back up more stuff

--- setup/files/scripts/home_dir_backup.sh
+++ setup/files/scripts/home_dir_backup.sh
@@ -1,13 +1,43 @@
 #!/bin/bash
+set -e
+
 if [ -z "$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport /usr/sbin/airport -I | grep MDNetwork)" ]; then
     rsync -azv \
         --no-p --no-g --chmod=ugo=rwX \
         --delete \
         --exclude '/.Trash' \
-        --exclude '/Library' \
+        --exclude '/Library/Autosave Information' \
+        --exclude '/Library/IdentityServices' \
+        --exclude '/Library/Messages' \
+        --exclude '/Library/HomeKit' \
+        --exclude '/Library/Sharing' \
+        --exclude '/Library/Mail' \
+        --exclude '/Library/Accounts' \
+        --exclude '/Library/Safari' \
+        --exclude '/Library/Suggestions' \
+        --exclude '/Library/Containers/com.apple.VoiceMemos' \
+        --exclude '/Library/Containers/com.apple.archiveutility' \
+        --exclude '/Library/Containers/com.apple.Home' \
+        --exclude '/Library/Containers/com.apple.Safari' \
+        --exclude '/Library/Containers/com.apple.iChat' \
+        --exclude '/Library/Containers/com.apple.CloudDocs.MobileDocumentsFileProvider' \
+        --exclude '/Library/Containers/com.apple.mail' \
+        --exclude '/Library/Containers/com.apple.Notes' \
+        --exclude '/Library/Containers/com.apple.news' \
+        --exclude '/Library/Containers/com.apple.stocks' \
+        --exclude '/Library/Containers/com.docker.docker/Data/vms' \
+        --exclude '/Library/PersonalizationPortrait' \
+        --exclude '/Library/Preferences/com.apple.homed.notbackedup.plist' \
+        --exclude '/Library/Preferences/com.apple.homed.plist' \
+        --exclude '/Library/Preferences/com.apple.mail-shared.plist' \
+        --exclude '/Library/Metadata/CoreSpotlight' \
+        --exclude '/Library/Cookies' \
+        --exclude 'Library/Application Support' \
+        --exclude 'Library/Caches' \
+        --exclude 'Library/Developer' \
         --exclude '/Music/iTunes/iTunes Media/Apple Music' \
-        --exclude '/Paralels' \
         --exclude '/Documents/Virtual Machines.localized' \
+        --exclude '/Parallels' \
         --exclude "/Pictures/Photos Library.photoslibrary/" \
         --timeout=15 \
         ~/ "$(whoami)-backups@192.168.81.18:/mnt/backups/computers/$(hostname)/$(whoami)/"
