80b99d0b0616e56bd27a8645179504c1525dde50
Mike Newswanger <mike@mikenewswanger.com>
2021-11-15 16:08:08 -0500

    Update backup exclusions and installed packages

--- setup/files/scripts/home_dir_backup.sh
+++ setup/files/scripts/home_dir_backup.sh
@@ -6,16 +6,13 @@ if [ -z "$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Curre
         --no-p --no-g --chmod=ugo=rwX \
         --delete \
         --exclude '/.Trash' \
+        --exclude '/Documents/Virtual Machines.localized' \
+        --exclude '/Library/Accounts' \
+        --exclude '/Library/Application Support' \
         --exclude '/Library/Assistant' \
         --exclude '/Library/Autosave Information' \
-        --exclude '/Library/IdentityServices' \
-        --exclude '/Library/Messages' \
-        --exclude '/Library/HomeKit' \
-        --exclude '/Library/Sharing' \
-        --exclude '/Library/Mail' \
-        --exclude '/Library/Accounts' \
-        --exclude '/Library/Safari' \
-        --exclude '/Library/Suggestions' \
+        --exclude '/Library/Biome' \
+        --exclude '/Library/Caches' \
         --exclude '/Library/Containers/com.apple.VoiceMemos' \
         --exclude '/Library/Containers/com.apple.archiveutility' \
         --exclude '/Library/Containers/com.apple.Home' \
@@ -28,17 +25,26 @@ if [ -z "$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Curre
         --exclude '/Library/Containers/com.apple.stocks' \
         --exclude '/Library/Containers/com.apple.corerecents.recentsd/Data/Library/Recents' \
         --exclude '/Library/Containers/com.docker.docker/Data/vms' \
+        --exclude '/Library/Cookies' \
+        --exclude '/Library/CoreFollowUp' \
+        --exclude '/Library/Developer' \
+        --exclude '/Library/Group Containers/group.com.apple.secure-control-center-preferences' \
+        --exclude '/Library/Group Containers/group.com.apple.notes' \
+        --exclude '/Library/HomeKit' \
+        --exclude '/Library/IdentityServices' \
+        --exclude '/Library/Mail' \
+        --exclude '/Library/Messages' \
+        --exclude '/Library/Metadata/CoreSpotlight' \
+        --exclude '/Library/Safari' \
+        --exclude '/Library/Sharing' \
+        --exclude '/Library/Shortcuts' \
+        --exclude '/Library/Suggestions' \
         --exclude '/Library/PersonalizationPortrait' \
         --exclude '/Library/Preferences/com.apple.homed.notbackedup.plist' \
         --exclude '/Library/Preferences/com.apple.homed.plist' \
         --exclude '/Library/Preferences/com.apple.mail-shared.plist' \
-        --exclude '/Library/Metadata/CoreSpotlight' \
-        --exclude '/Library/Cookies' \
-        --exclude 'Library/Application Support' \
-        --exclude 'Library/Caches' \
-        --exclude 'Library/Developer' \
+        --exclude '/Library/StatusKit' \
         --exclude '/Music/iTunes/iTunes Media/Apple Music' \
-        --exclude '/Documents/Virtual Machines.localized' \
         --exclude '/Parallels' \
         --exclude "/Pictures/Photos Library.photoslibrary/" \
         --timeout=15 \
